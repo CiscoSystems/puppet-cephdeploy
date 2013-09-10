cae78255dddd9e2209de83e9f4529893fc1534b9
root <root@build-server.(none)>
2013-09-10 16:44:35 -0700

    changed from facter hostname to specified hostname from site.pp

--- manifests/osd.pp
+++ manifests/osd.pp
@@ -10,7 +10,7 @@ define cephdeploy::osd(
   exec { "gatherkeys_$disk":
     cwd     => '/etc/ceph/bootstrap',
     user    => $user,
-    command => "/bin/rm -f /etc/ceph/bootstrap/ceph.log && /usr/local/bin/ceph-deploy gatherkeys $::hostname",
+    command => "/bin/rm -f /etc/ceph/bootstrap/ceph.log && /usr/local/bin/ceph-deploy gatherkeys $::mon_initial_members",
     require => [ Exec['install ceph'], File["/etc/sudoers.d/$user"], File['/etc/ceph/bootstrap/ceph.log'] ],
     unless  => '/usr/bin/test -e /etc/ceph/bootstrap/ceph.bootstrap-osd.keyring',
   }
