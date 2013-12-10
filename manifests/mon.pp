496a1ad24a483a7f29390fb65a95f665c6662af4
root <dotalton@cisco.com>
2013-12-09 17:10:56 -0700

    added manual copying of ceph keys

--- manifests/mon.pp
+++ manifests/mon.pp
@@ -14,6 +14,21 @@ class cephdeploy::mon(
     provider => shell,
   }
 
+  if $ceph_primary_mon == $::hostname {
+    exec { 'copy keys':
+      path => '/bin:/usr/bin',
+      command => "cp /var/lib/ceph/bootstrap-mds/$cluster.keyring /home/$user/bootstrap/$cluster.bootstrap-mds.keyring &&
+cp /var/lib/ceph/bootstrap-osd/$cluster.keyring /home/$user/bootstrap/$cluster.bootstrap-osd.keyring &&
+cp /etc/ceph/ceph.client.admin.keyring /home/$user/bootstrap/ceph.client.admin.keyring &&
+cp /var/lib/ceph/mon/ceph-$ceph_primary_mon/keyring /home/$user/bootstrap/ceph.mon.keyring &&
+chown $user:$user /home/$user/bootstrap/* &&
+chmod 644 /home/$user/bootstrap/*
+",
+      require => Exec['create mon'],
+      unless => "/usr/bin/test -e /home/$user/bootstrap/$cluster.bootstrap-osd.keyring",
+    }
+  }
+
   exec {'iptables mon':
     command => "/sbin/iptables -A INPUT -i $ceph_public_interface -p tcp -s $ceph_public_network --dport 6789 -j ACCEPT",
     unless  => '/sbin/iptables -L | grep "tcp dpt:6789"',
