89c6fde6368a8b1c98d9e4c4edd52973e4046fad
Chris Ricker <chricker@cisco.com>
2014-07-10 08:35:52 -0400

    dpkg call fails on RHEL
    Replace dpkg-based test with platform-independent test.
    
    Closes-Bug: #1340164
    (cherry picked from commit 47960d882e8b7a50a95e9033ba5398ef209d1463)
    
    Conflicts:
    	manifests/init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -180,7 +180,7 @@ class cephdeploy(
   exec { 'install ceph':
     cwd     => "/home/$ceph_deploy_user/bootstrap",
     command => "/usr/bin/ceph-deploy install --no-adjust-repos $::hostname",
-    unless  => '/usr/bin/dpkg -l | grep ceph-common',
+    unless  => '/usr/bin/test -f /usr/bin/ceph',
     require => [ Package['ceph-deploy'], File['ceph.mon.keyring'], File["/home/$ceph_deploy_user/bootstrap"] ],
   }
 
